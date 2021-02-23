c5b9d28fa2704027065cb3b90f11a9eb7461b669
Aksel Sjögren <akselsjogren@users.noreply.github.com>
2021-02-23 10:03:33 +0100

    Update post_notify

--- examples/hooks/post_notify.sh
+++ examples/hooks/post_notify.sh
@@ -11,17 +11,17 @@ CEBACKUP_BACKUP_PATH=$CEBACKUP_BACKUP_PATH
 CEBACKUP_SOURCES=$CEBACKUP_SOURCES
 
 if [ $CEBACKUP_OK -eq 1 ]; then
-    if [ $CEBACKUP_BACKUP_CREATED -eq 1 ]; then
-        msg="Created: $CEBACKUP_BACKUP_PATH\nFrom $(wc -l < "$CEBACKUP_SOURCES") source paths."
-    else
-        msg="Up to date archive already exists ($CEBACKUP_BACKUP_PATH).\nFrom $(wc -l < "$CEBACKUP_SOURCES") source paths."
-    fi
-    notify-send -u normal -a cebackup "Backup completed" "$msg"
+	if [ $CEBACKUP_BACKUP_CREATED -eq 1 ]; then
+		msg="Created: $CEBACKUP_BACKUP_PATH\nFrom $(wc -l < "$CEBACKUP_SOURCES") source paths."
+	else
+		msg="Up to date archive already exists ($CEBACKUP_BACKUP_PATH).\nFrom $(wc -l < "$CEBACKUP_SOURCES") source paths."
+	fi
+	notify-send -u normal -a cebackup --icon=emblem-package "Backup completed" "$msg"
 else
-    if [ -e "$CEBACKUP_LOGFILE" ]; then
-        msg="Logfile: $CEBACKUP_LOGFILE\n\nLast logs: $(tail -5 "$CEBACKUP_LOGFILE")"
-    else
-        msg="backup failed"
-    fi
-    notify-send -u critical -a cebackup "Backup failed" "$msg"
+	if [ -e "$CEBACKUP_LOGFILE" ]; then
+		msg="Logfile: $CEBACKUP_LOGFILE\n\nLast logs: $(tail -5 "$CEBACKUP_LOGFILE")"
+	else
+		msg="backup failed"
+	fi
+	notify-send -u critical -a cebackup --icon=dialog-error "Backup failed" "$msg"
 fi
